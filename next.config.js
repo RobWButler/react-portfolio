49f651e83f45e4c7f39281e4b548b3579c56f216
Robert Butler <robwbutler@gmail.com>
2019-08-25 19:30:55 -0400

    static export

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+module.exports = {
+    exportPathMap: () => ({
+      '/': { page: '/' },
+      '/home': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/portfolio': { page: '/portfolio' },
+    }),
+    assetPrefix: isProd ? '/new-portfolio' : '',
+  }
+
