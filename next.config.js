47305d8db3c5140f1013856a5805e842959ce444
Robert Butler <robwbutler@gmail.com>
2019-08-25 20:30:47 -0400

    link alias fix from dmitryaa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const debug = process.env.NODE_ENV !== "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    exportPathMap: () => ({
-      '/' : {page: '/'},
-      '/index': { page: '/' },
-      '/home': { page: '/' },
-      '/about': { page: '/about' },
-      '/contact': { page: '/contact' },
-      '/portfolio': { page: '/portfolio' },
-    }),
-    assetPrefix: !debug ? 'https://robwbutler.github.io/new-portfolio/' : '',
+  assetPrefix: isProduction ? '/new-portfolio' : '',
+  publicRuntimeConfig: {
+    // used in '/components/Link.js/', for more details go to the component itself
+    linkPrefix: isProduction ? '/new-portfolio' : ''
   }
-
+};
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
+import Link from '../components/Link';
 import Page from '../layouts/main';
 import PortCard from '../components/Portcards/PortCard'
 import Cardlist from "../components/Portcards/cardlist";
