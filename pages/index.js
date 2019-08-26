7ac6ee44bd692bc591464beb8e45f3ea705d2690
Robert Butler <robwbutler@gmail.com>
2019-08-25 23:21:05 -0400

    starting over

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Page from "next/link"
+import Page from "../layouts/main"
 import NavCard from "../components/NavCards/NavCard"
 
 class Home extends React.Component {
