a023dab0c98f0b28acfb6d9ef3e1ba12a31d039e
Robert Butler <robwbutler@gmail.com>
2019-08-25 20:52:30 -0400

    more

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 const isProduction = process.env.NODE_ENV === 'production';
+module.exports = {
+  exportPathMap: async function() {
+    const paths = {
+      "/": { page: "/" },
+      "/index": { page: "/" },
+      "/about": { page: "/about" },
+      "/contact": { page: "/contact" },
+      "/portfolio": { page: "/portfolio" }
+    };
 
+    return paths;
+  }
+};
 module.exports = {
   assetPrefix: isProduction ? '/new-portfolio' : '',
   publicRuntimeConfig: {
     // used in '/components/Link.js/', for more details go to the component itself
     linkPrefix: isProduction ? '/new-portfolio' : ''
   }
-};
+}; 
